4e2ce487542cb3ccd6f918e12102cd59ddf89c25
Samson Osiomwan <81101034+samsonosiomwan@users.noreply.github.com>
2023-05-13 17:19:04 +0100

    deleting the secondScreen

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -12,9 +12,27 @@ class SecondScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
           appBar: AppBar(),
-          body: Column(children: [
-            
-          ])),
+          body: widget(
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  quoteText,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10.0),
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Text(
+                    "- $quoteAuthor",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ]),
+          )),
     );
   }
 }
